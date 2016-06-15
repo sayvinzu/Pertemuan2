023b69fea90b76a5430cbb804d220d336e8936a2
sayvinzu <syaifulamriyahya@gmail.com>
2016-06-15 21:54:13 +0700

    menambah activity kalkulator

--- app/src/main/java/com/example/sayvinzu/pertemuan2/MainActivity.java
+++ app/src/main/java/com/example/sayvinzu/pertemuan2/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.sayvinzu.pertemuan2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button BtSet;
+    private TextView TxtJln;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BtSet = (Button) findViewById(R.id.Bt_set);
+        TxtJln = (TextView) findViewById(R.id.text_jalan);
+
+        BtSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TxtJln.setText(Alamat());
+                BtSet.setText("RESET");
+            }
+        });
     }
+
+    public String Alamat (){
+        String hasil = "Jl. Cakalang No. 209 A, Bilmbing";
+        return hasil;
+    }
+
 }
